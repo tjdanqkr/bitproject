04598dcac92b5534a77bdf6922e0678e70c09385
tjdanqkr <tjdanqkr@gmail.com>
2020-05-08 16:39:35 +0900

    join

--- front/src/page/Join2.js
+++ front/src/page/Join2.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import {useForm} from 'react-hook-form';
-
+import { useForm } from "react-hook-form";
 
 function Join2() {
   const [idcheck, setIdcheck] = useState(false);
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
+  const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+
   const [repw, setRepw] = useState("");
   const [checkmessage, setCheckMessage] = useState("");
-  const {register, handleSubmit, reset, errors} = useForm();
+  const { register, handleSubmit, reset, errors } = useForm();
   const idch = (e) => {
     //e.preventdefault();
     //setIdcheck(true);
@@ -29,35 +31,38 @@ function Join2() {
     //   });
     console.log(id);
   };
-  const OnSubmit = ((e) =>{
+  const OnSubmit = (e) => {
     //e.preventdefault();
     if (pw === repw) {
-    //   const post = {
-    //     id: id,
-    //     pw: pw
-    //   };
+      //   const post = {
+      //     id: id,
+      //     pw: pw
+      //   };
       console.log("sus");
-    }else
-        console.log("fail");
-  });
+    } else console.log("fail");
+  };
 
-
-  return(
+  return (
     <div>
       <form onSubmit={handleSubmit(OnSubmit)}>
-        <input type="text"  name="Id" onChange={(e) => setId(e.target.value)} />
-        
-        {(idcheck) ? ({ checkmessage } ): (null)}
+        <input type="text" name="Id" onChange={(e) => setId(e.target.value)} />
+
+        {idcheck ? { checkmessage } : null}
         <input
-          type="password" name="Pw"
+          type="password"
+          name="Pw"
           onChange={(e) => setPw(e.target.value)}
         />
         <input
-          type="password" name="rePw"
+          type="password"
+          name="rePw"
           onChange={(e) => setRepw(e.target.value)}
         />
-        <input type="submit" value="Join" />
+
+        <input type="text" name="name" onChange={(e) => setId(e.target.value)} />
+        <input type="text" name="age" onChange={(e) => setId(e.target.value)} />
         
+        <input type="submit" value="Join" />
       </form>
       <button onClick={idch}>아이디 중복 검사</button>
     </div>
